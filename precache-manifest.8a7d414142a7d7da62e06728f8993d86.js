c4292e54be8091f75031568ba618cc70278e25fc
StachRach <86355502+StachRach@users.noreply.github.com>
2023-03-12 22:10:25 +0100

    Updates

---
+++ precache-manifest.8a7d414142a7d7da62e06728f8993d86.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "383bc7c8f561a711f1a0",
+    "url": "/SPA-with-React-Router/static/css/main.b2beabe0.chunk.css"
+  },
+  {
+    "revision": "383bc7c8f561a711f1a0",
+    "url": "/SPA-with-React-Router/static/js/main.82089506.chunk.js"
+  },
+  {
+    "revision": "e1d22124a9b105f52fa8",
+    "url": "/SPA-with-React-Router/static/js/runtime~main.c2c9ce9e.js"
+  },
+  {
+    "revision": "cac3784fdbb2e513785c",
+    "url": "/SPA-with-React-Router/static/js/2.9a9fd2af.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/SPA-with-React-Router/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/SPA-with-React-Router/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/SPA-with-React-Router/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "87601e4e8f7547aa68a43a4cf6cd34be",
+    "url": "/SPA-with-React-Router/static/media/header4.87601e4e.jpeg"
+  },
+  {
+    "revision": "dd481af3a2f9dd6c6254bfe7e8b762f5",
+    "url": "/SPA-with-React-Router/static/media/404.dd481af3.jpg"
+  },
+  {
+    "revision": "650a7a8dbd855b630a8fb083eea72914",
+    "url": "/SPA-with-React-Router/index.html"
+  }
+];
