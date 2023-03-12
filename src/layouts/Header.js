efcc92877677ba3e950ca599d27b6b38fe2f787a
StachRach <86355502+StachRach@users.noreply.github.com>
2023-03-12 22:35:10 +0100

    Deployment

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -8,10 +8,11 @@ const Header = () => {
    return (
       <>
          <Routes>
-            <Route path="/" element={<Image index={0}/>}/>
+            <Route path={"/"} element={<Image index={0}/>}/>
             <Route path="/products" element={<Image index={1}/>}/>
             <Route path="/contact" element={<Image index={2}/>}/>
             <Route path="/admin" element={<Image index={3}/>}/>
+            <Route path="/login" element={<Image index={3}/>}/>
             <Route path="*" element={<Image index={0}/>}/>
          </Routes>
       </>
